5c02522f7686233c5e9ebf355cfc0e69c3c35bdb
Nicholas <nicholassamuel@NICHOLASSAMUEL12.com>
2023-03-31 09:00:46 +0700

    initial commit

---
+++ uts1.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'portfolio-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/images/background.jpg',
+  '/images/bitmap.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache berhasil dibuat');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request)
+          .then(function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+});
